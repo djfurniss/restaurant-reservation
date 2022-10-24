911978737867c676023a469f503184d7fa39dc2b
Denasia <dfurniss@ymail.com>
2022-10-24 19:02:37 -0400

    useEffect rearranged to take care of a manually typed query for the date; added a  new function updateQuery that's used in the useEffect as well as a helper function

--- front-end/src/dashboard/Dashboard.js
+++ front-end/src/dashboard/Dashboard.js
@@ -21,27 +21,44 @@ function Dashboard({date, setDate, today, previous, next}) {
   const [reservationsError, setReservationsError] = useState(null);
   
 // --- useEffect ---
-  useEffect(() => {
-    function updateQuery(){
-      query.set("date", date);
-      var newRelativePathQuery = `${window.location.pathname}?${query.toString()}`
-      history.push(newRelativePathQuery);
-    };
+useEffect(() => {
+  function loadDashboard() {
+    const abortController = new AbortController();
+    setReservationsError(null);
+    listReservations({ date }, abortController.signal)
+      .then(setReservations)
+      .catch(setReservationsError);
+    listTables(abortController.signal)
+      .then(setTables)
+    return () => abortController.abort();
+  };
+  
+  // if the page is loaded with no query, one is generated with the default date
+  if (!query.get("date")){
+    query.set("date", date)
+    // these two steps are taken to make sure the URL is updated and not just the value of the query
+    var newRelativePathQuery = `${window.location.pathname}?${query.toString()}`
+    history.push(newRelativePathQuery);
+  };
+  // otherwise, if there's a date in the query that's not the default date, this function will make sure the date state is updated to the date that's manually typed in the query
+  updateQuery(query.get("date"));
+  loadDashboard();
+}, [date]); 
 
-    function loadDashboard() {
-        const abortController = new AbortController();
-        setReservationsError(null);
-        listReservations({ date }, abortController.signal)
-          .then(setReservations)
-          .catch(setReservationsError);
-        listTables(abortController.signal)
-          .then(setTables)
-        return () => abortController.abort();
-    };
-
-    updateQuery();
-    loadDashboard();
-  }, [date]);
+// --- helper functions ---
+  /**
+   * Since the query can be updated by changing state with clicking buttons OR by manually chaning the url, 
+   * this function is defined outside of the useEffect to be used in it but also to be used by any handlers that need to update the query
+   * 
+   * @param {*} newDate 
+   */
+function updateQuery (newDate) {
+  query.set("date", newDate)
+  setDate(query.get("date"))
+  // these two steps are taken to make sure the URL is updated and not just the value of the query
+  var newRelativePathQuery = `${window.location.pathname}?${query.toString()}`
+  history.push(newRelativePathQuery);
+};
     
 // --- return ---
   return (
@@ -49,9 +66,9 @@ function Dashboard({date, setDate, today, previous, next}) {
       <h1>Dashboard</h1>
       <ErrorAlert error={reservationsError} />
 
-      <button onClick={()=>setDate(previous(date))}>Previous Day</button>
-      <button onClick={()=>setDate(today())}>Today</button>
-      <button onClick={()=>setDate(next(date))}>Next Day</button>
+      <button onClick={()=>updateQuery(previous(date))}>Previous Day</button>
+      <button onClick={()=>updateQuery(today())}>Today</button>
+      <button onClick={()=>updateQuery(next(date))}>Next Day</button>
 
       <div className="d-md-flex mb-3">
         <h4 className="mb-0">Reservations for {date}</h4>
