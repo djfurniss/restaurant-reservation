5fa7b68abaf1595ef0a03f22b25257690a82f885
Denasia <dfurniss@ymail.com>
2022-10-24 08:14:01 -0400

    attributes added to tables to be found by tests; each reservation renders a seat button

--- front-end/src/tables/OneTable.js
+++ front-end/src/tables/OneTable.js
@@ -3,7 +3,7 @@ export default function OneTable ({ table }) {
         <tr>
             <td>{table.table_name}</td>
             <td>{table.capacity}</td>
-            <td>{table.reservation_id ? "Occupied" : "Free"}</td>
+            <td data-table-id-status={table.table_id}>{table.reservation_id ? "Occupied" : "Free"}</td>
         </tr>
     )
 };
