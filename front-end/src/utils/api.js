a1778c930078a888027504aba2c481d2a90d1749
Denasia <dfurniss@ymail.com>
2022-10-26 18:24:06 -0400

    took out the updateResStatus function that called to the reservations/:reservation_id/status endpoint since it's not to be used, only to be tested by backend

--- front-end/src/utils/api.js
+++ front-end/src/utils/api.js
@@ -84,21 +84,6 @@ export async function createReservation(newReservation, signal){
     .then(formatReservationTime);
 };
 
-export async function updateResStatus(reservation_id, status, signal){
-  const url = new URL(`${API_BASE_URL}/reservations/${reservation_id}/status`);
-
-  const options = {
-    method: "PUT",
-    headers,
-    body: JSON.stringify({ data: {status} }),
-    signal,
-   };
-
-   return await fetchJson(url, options, [])
-    .then(formatReservationDate)
-    .then(formatReservationTime);
-};
-
 export async function listTables(signal){
   const url = new URL(`${API_BASE_URL}/tables`);
   return await fetchJson(url, { headers, signal }, [])
