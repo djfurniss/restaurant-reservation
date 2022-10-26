935ceaa4763121f78a6742a17b233638aaac7d4c
Denasia <dfurniss@ymail.com>
2022-10-26 18:26:13 -0400

    conditionally render seat button and reservation information based off status

--- front-end/src/reservations/OneReservation.js
+++ front-end/src/reservations/OneReservation.js
@@ -4,15 +4,15 @@ export default function OneReservation({ resv }){
     const history = useHistory();
 
     return (
-        resv.status != "finished" && 
+        resv.status !== "finished" && 
         <tr>
             <td>{`${resv.first_name} ${resv.last_name}`}</td>
             <td>{resv.mobile_number}</td>
             <td>{resv.reservation_date}</td>
             <td>{resv.reservation_time}</td>
             <td>{resv.people}</td>
-            <td data-reservation-id-status={resv.reservation_id} >{resv.status}</td>
-            {resv.status == "booked" &&
+            <td data-reservation-id-status={`${resv.reservation_id}`} >{resv.status}</td>
+            {resv.status === "booked" &&
             <td>
                 <button 
                     onClick={()=>history.push(`/reservations/${resv.reservation_id}/seat`)}
