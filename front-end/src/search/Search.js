729459563a92dfd3c8cf7152c301bf74181f195a
Denasia <dfurniss@ymail.com>
2022-10-27 11:37:27 -0400

    search page render

---
+++ front-end/src/search/Search.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import { findReservation, findReservationByNumber } from "../utils/api";
+import ListReservations from "../reservations/ListReservations";
+
+export default function Search(){
+    const [reservations, setReservations] = useState([]);
+    const [number, setNumber] = useState("")
+
+    const handleInputChange = ({target: {value}}) => setNumber(value);
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        findReservationByNumber(number)
+            .then(setReservations)
+            .then(()=>setNumber(""))
+    };
+
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <input 
+                    name="mobile_number"
+                    value={number}
+                    onChange={handleInputChange}
+                    placeholder="Enter a customer's phone number"/>
+                <button type="submit">Find</button>
+            </form>
+
+            {reservations.length ?
+            <ListReservations reservations={reservations}/> :
+            <h1>No reservations found</h1>}
+        </div>
+    )
+};
