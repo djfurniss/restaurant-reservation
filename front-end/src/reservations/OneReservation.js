b823190795cd253ffefb22ac42480a025754b5ad
Denasia <dfurniss@ymail.com>
2022-10-28 16:24:47 -0400

    imported updateStatus from utils/api; conditionally render reservations for dashboard (doesn't render finished or cancelled statuses); conditionally render edit button for booked statuses only; cancel button for all reservations

--- front-end/src/reservations/OneReservation.js
+++ front-end/src/reservations/OneReservation.js
@@ -1,10 +1,11 @@
 import { useHistory } from "react-router";
+import { updateStatus } from "../utils/api";
 
-export default function OneReservation({ resv }){
+export default function OneReservation({ resv, purpose }){
     const history = useHistory();
 
     return (
-        resv.status !== "finished" && 
+        purpose === "dashboard" && resv.status === "finished" || resv.status === "cancelled" ? null :
         <tr>
             <td>{`${resv.first_name} ${resv.last_name}`}</td>
             <td>{resv.mobile_number}</td>
@@ -20,6 +21,23 @@ export default function OneReservation({ resv }){
                     Seat
                 </button>
             </td>}
+            {resv.status === "booked" &&
+             <td>
+                <button 
+                    onClick={()=>history.push(`/reservations/${resv.reservation_id}/edit`)}
+                    href={`/reservations/${resv.reservation_id}/edit`}>
+                    Edit
+                    </button>
+            </td>}
+            <td>
+                <button 
+                    onClick={()=>{
+                        window.confirm("Do you want to cancel this reservation? This cannot be undone.") 
+                        && updateStatus(resv.reservation_id, "cancelled") 
+                        && history.go(0)
+                    }}
+                    data-reservation-id-cancel={resv.reservation_id}>Cancel</button>
+            </td>
         </tr>
     )
 };
