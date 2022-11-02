46bee6defd573a1d2057770e82f991a12885e0a6
Denasia <dfurniss@ymail.com>
2022-11-02 16:04:25 -0400

    cleaning code for consistancy and adding comments to EditReservation

--- front-end/src/reservations/EditReservation.js
+++ front-end/src/reservations/EditReservation.js
@@ -22,6 +22,7 @@ export default function EditReservation({ setDate }){
     useEffect(()=>{
         const abortController = new AbortController();
         readReservation(reservation_id, abortController.signal)
+        //destructuring only the variables needed from the DB, because all columns like created_at and updated_at come back from the request
             .then(({ first_name, last_name, mobile_number, reservation_date, reservation_time, people }) => {
                 setFormData({first_name, last_name, mobile_number, reservation_date, reservation_time, people})
             })
@@ -35,6 +36,7 @@ export default function EditReservation({ setDate }){
         const abortController = new AbortController();
         updateReservation(formData, reservation_id, abortController.signal)
             .then(({ reservation_date })=> {
+                //setDate is used to set date's state to the reservation's date so when the user is pushed back to the dashboard, it loads with the new date of the reservation IF it was updated.
                 setDate(reservation_date)
                 history.push("/")
             })
@@ -45,13 +47,13 @@ export default function EditReservation({ setDate }){
 // --- return ---
     return (
         <div>
-            <h1>Editing reservation #{reservation_id}</h1>
-            <ErrorAlert error={formErr}/>
-            <ReservationForm 
-                purpose="edit" 
-                formData={formData} 
-                setFormData={setFormData}
-                handleSubmit={handleSubmit}/>
+        <h1>Editing reservation #{reservation_id}</h1>
+        <ErrorAlert error={formErr}/>
+        <ReservationForm 
+            purpose="edit" 
+            formData={formData} 
+            setFormData={setFormData}
+            handleSubmit={handleSubmit}/>
         </div>
     )
 };
