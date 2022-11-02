475be0043eaf09cf2d4fee023f3effb3faafeb64
Denasia <dfurniss@ymail.com>
2022-11-02 16:27:57 -0400

    indenting and fixing semicolons for consitancy

--- back-end/src/tables/tables.controller.js
+++ back-end/src/tables/tables.controller.js
@@ -65,53 +65,52 @@ const { updateStatus: updateResStatus } = require("../reservations/reservations.
     };
 
     const hasValidName = (req, res, next) => {
-        const { table_name } = req.body.data
+        const { table_name } = req.body.data;
         if (table_name.length < 2){
-            next({status: 400, message: "table_name must be at least 2 characters."})
+            next({status: 400, message: "table_name must be at least 2 characters."});
         };
         next();
     };
 
     const hasValidCapacity = (req, res, next) => {
-        const { capacity } = req.body.data
-
-        if (typeof capacity != "number") next({status: 400, message:"capacity must be a number"})
-        if (capacity <=0 ) next({status: 400, message: "capacity must be greater than 0"})
+        const { capacity } = req.body.data;
+        if (typeof capacity != "number") next({status: 400, message:"capacity must be a number"});
+        if (capacity <=0 ) next({status: 400, message: "capacity must be greater than 0"});
         next();
     };
 
 // --- router level middleware ---
     async function list (req, res) {
-        const data  = await service.list()
-        res.json({ data })
+        const data  = await service.list();
+        res.json({ data });
     };
 
     async function create (req, res) {
         const data = await service.create(req.body.data);
-        res.status(201).json({ data })
+        res.status(201).json({ data });
     };
 
     async function update (req, res) {
         // the table_id from params goes through verification and is assigned into locals if it's valid and exists
-        const { table_id } = res.locals.table
+        const { table_id } = res.locals.table;
         // the reservation from the request body also goes through verification and is assigned to locals if it's valid and exists
-        const { reservation_id } = res.locals.reservation
+        const { reservation_id } = res.locals.reservation;
         // once everything goes through validation, the update service is called
         // await updateResStatus(reservation_id)
-        const updatedTable = await service.update(table_id, reservation_id)
+        const updatedTable = await service.update(table_id, reservation_id);
         // ? the tests don't make their own api call to /reservation/:reservation_id/status so I had to import the service function from the reservations.service into this one api call to update the reservation's status
         await updateResStatus(reservation_id, "seated");
         // the table is returned with the updated information
-        res.json({data: updatedTable})
+        res.json({data: updatedTable});
         // next();
     };
 
     async function destroy (req, res) {
-        const { table_id, reservation_id } = res.locals.table
-        await updateResStatus(reservation_id, "finished")
-        await service.finishTable(table_id)
+        const { table_id, reservation_id } = res.locals.table;
+        await updateResStatus(reservation_id, "finished");
+        await service.finishTable(table_id);
         // res.sendStatus(204)
-        res.json({data: "done!"})
+        res.json({data: "done!"});
     };
 
 // --- exports ---
